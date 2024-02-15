df9b68a614ad2c58fb2d9a7ab501734e6e4dd004
CodeRAFCE <seshan.io@prismscale.com>
2024-02-15 21:01:19 +0530

    increasing padding inline

--- src/components/MaxWidthWrapper.jsx
+++ src/components/MaxWidthWrapper.jsx
@@ -3,7 +3,7 @@ import {cn} from "../lib/utils";
 
 const MaxWidthWrapper = ({className, children}) => {
 	return (
-		<div className={cn("w-full px-2.5 md:px-0 max-w-[1440px] mx-auto", className)}>{children}</div>
+		<div className={cn("w-full px-4 md:px-0 max-w-[1440px] mx-auto ", className)}>{children}</div>
 	);
 };
 
