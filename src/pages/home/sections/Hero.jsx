ebeb59c61f6f0f9374638e1ca510b9760fe89441
CodeRAFCE <seshan.io@prismscale.com>
2024-02-23 22:24:22 +0530

    updating image

--- src/pages/home/sections/Hero.jsx
+++ src/pages/home/sections/Hero.jsx
@@ -8,7 +8,16 @@ const Hero = () => {
 				<AnotherLand />
 
 				<div className="relative flex flex-col">
-					<div className="container w-full h-full flex items-center justify-center">
+					<div className="w-full h-full items-center justify-center hidden xl:flex">
+						<img
+							src="/assets/gifs/hero.gif"
+							alt="image_01"
+							loading="lazy"
+							className="object-fill md:-m-6"
+						/>
+					</div>
+
+					<div className="container w-full h-full flex items-center justify-center xl:hidden">
 						<img
 							src="/assets/gifs/hero.gif"
 							alt="image_01"
