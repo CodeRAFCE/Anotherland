a9fada405e6959df6b5bc42545e90c1a68cce118
CodeRAFCE <seshan.io@prismscale.com>
2024-02-28 12:47:07 +0530

    fixing layout shifts

--- src/pages/home/sections/Hero.jsx
+++ src/pages/home/sections/Hero.jsx
@@ -9,42 +9,44 @@ const Hero = () => {
 
 				<div className="relative flex flex-col">
 					<div className="max-w-full w-full h-full items-center justify-center hidden xl:flex">
-						{/* <img
-							src="/assets/gifs/hero.gif"
-							alt="image_01"
-							loading="lazy"
-							className="object-fill md:-m-3"
-						/> */}
-						<video autoPlay loop className="w-full h-full" preload="auto" muted="true" playsInline>
+						<video
+							autoPlay
+							loop
+							className="w-full h-full"
+							preload="auto"
+							poster="/assets/images/thumbnail.webp"
+							muted
+							playsInline
+						>
 							<source src="/assets/hero.mp4" type="video/mp4" />
 							<img
-								src="/assets/images/thumbnail.webp"
+								src=""
 								alt="image_01"
 								loading="lazy"
 								className="object-fill object-center md:-m-3"
-								width="1280"
-								height="720"
 							/>
 						</video>
 					</div>
 
 					<div className="container w-full h-full flex items-center justify-center xl:hidden">
-						<video autoPlay loop className="w-full h-full" preload="auto" muted="true" playsInline>
+						<video
+							autoPlay
+							loop
+							className="w-full h-full"
+							poster="/assets/images/thumbnail.webp"
+							preload="auto"
+							muted="true"
+							playsInline
+						>
 							<source src="/assets/hero.mp4" type="video/mp4" />
 							<img
-								src="/assets/images/thumbnail.webp"
+								src=""
 								alt="image_01"
 								loading="lazy"
 								className="object-fill object-center md:-m-3"
 							/>
 						</video>
 					</div>
-					{/* <img
-						src="/assets/images/anotherland_image_01.png"
-						alt="image_01"
-						loading="lazy"
-						className=""
-					/> */}
 				</div>
 			</div>
 		</MaxWidthWrapper>
